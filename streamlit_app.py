2cffe7203f774775eea9549055aaf047054fc931
Borys Sushylnikov <107059967+BorysSushylnikov@users.noreply.github.com>
2022-06-09 11:50:30 +0200

    Update streamlit_app.py
    fruityvice_response, connect to url

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,20 @@
 import streamlit
 import pandas
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
 streamlit.title("My Parents New Healthy Dinner")
 streamlit.header("Breakfast Menu")
-streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
-streamlit.text("🥗 Kale, Spinach & Ricket Smoothie")
-streamlit.text("🐔 Hard-Boiled Free-Range Egg")
-streamlit.text("🥑🍞 Avocado Toast")
+streamlit.text(fruityvice_response)
+
+# streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+# streamlit.text("🥗 Kale, Spinach & Ricket Smoothie")
+# streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+# streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
