3e9ebaa849407ff963fe6fa99cf3dede80b43ec8
Borys Sushylnikov <107059967+BorysSushylnikov@users.noreply.github.com>
2022-06-13 15:22:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,16 +48,22 @@ except URLError as e:
 streamlit.write('The user entered', fruit_choice)
 # streamlit.text(fruityvice_response.json()) # just writes the data to the screen 
 
-
+streamlit.header("My fruit load list contains:")
+# Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+  
+# Add a button to load the fruit
+if streamlit.button("Get fruit Load List"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = my_cur.fetchall()
+  streamlit.dataframe(my_data_rows)
+  
+  
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
-my_data_rows = my_cur.fetchall()
-
-streamlit.header("My fruit load list contains:")
-streamlit.dataframe(my_data_rows)
 
 # New Section to display fruityvice api response pt 2
 fruit_choice1 = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
